105bb9de9aaf994492c56869bf7cde104579bbb0
Erin Anderson <erinmorgan@ufl.edu>
2016-11-02 09:39:41 -0400

    First Commit

---
+++ PeoplePlus/app/main.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Main', main);
+
+    function main() {
+        var vm = this;
+        vm.un = 'Username';
+        vm.pw = 'Password';
+        vm.fn = 'First Name';
+        vm.ln = 'Last Name';
+        vm.en = 'Employee Number';
+        vm.tn = 'Tracking Number';
+        vm.rpw = 'Repeat Password';
+    }
+
+})();
